c0e53ad153e74f90046acc935b2e24aa1a764ca0
nittyan <kaziotore@gmail.com>
2017-12-20 12:16:14 +0000

    refactoring

--- uploader.py
+++ uploader.py
@@ -4,12 +4,11 @@ import boto3
 class Uploader(object):
 
     def __init__(self, bucket_name):
-        self._bucket_name = bucket_name
-        self._bucket = self._get_bucket()
+        self._bucket = self._get_bucket(bucket_name)
 
-    def _get_bucket(self):
+    def _get_bucket(self, bucket_name):
         s3 = boto3.resource('s3')
-        return s3.Bucket(self._bucket_name)
+        return s3.Bucket(bucket_name)
         
     def upload(self, local_path, upload_path):
         self._bucket.put_object(
